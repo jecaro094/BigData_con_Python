e080a1204267645eea34771357abd7cb5c1744ab
jecaro094 <jecarobd@gmail.com>
2020-02-16 13:00:57 +0100

    Python changes for ejercicio2.py

--- 02 Web Scraping/ejercicio2.py
+++ 02 Web Scraping/ejercicio2.py
@@ -3,7 +3,6 @@ import urllib.parse
 import urllib.request
 import json
 
-
 serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 def devuelve_coordenadas():
@@ -25,10 +24,12 @@ def devuelve_coordenadas():
     print('lat:', lat, 'lng', lng)
     location = js['results'][0]['formatted_address']
     print(location + '\n')
-    return lat, lng
+    return lat, lng, address
 
-def catastro(lat_to_search, lon_to_search):
+def catastro(lat_to_search, lon_to_search,ciudad):
     from selenium import webdriver
+    import selenium.common.exceptions  
+
     driver = webdriver.Chrome()
     url = "https://www1.sedecatastro.gob.es/CYCBienInmueble/OVCBusqueda.aspx"
     driver.get(url)
@@ -47,27 +48,23 @@ def catastro(lat_to_search, lon_to_search):
     ActionChains(driver).move_to_element(lon).click(lon) # Para provocar el click en la caja y que salte el JavaScript
     lon.send_keys(str(lon_to_search))
 
-    # Pulsamos botones
-    datos = driver.find_element_by_id("ctl00_Contenido_btnDatos")
-    datos.click()
+    try:
+        # Pulsamos botones
+        datos = driver.find_element_by_id("ctl00_Contenido_btnDatos")
+        datos.click()
 
-
-    html = driver.find_element_by_xpath("/html") # / se usa para referirnos al pricipio del documento (camino absoluto)
-    print("##################print(html.text)#####################")
-    print(html)
-
-    hijos = driver.find_elements_by_xpath("/html/body/*") # No nos importa el nombre del elto en concreto
-    print("####################print(element.tag_name)##################")
-    for i, element in enumerate(hijos):
-        print(f'Elemento {i}: ' + element.text + '\n')
-
-
-
+        #html = driver.find_element_by_id("ctl00_Contenido_HtmlTodos")
+        #html = driver.find_element_by_id("datosinmueble")
+        html = driver.find_element_by_xpath("/html")
+        print(html.text)
+        
+    except:
+        print(f"No se encontraron datos de catastro para la ciudad de '{ciudad}'")
 
     # Cuando hayamos terminado de usar Selenium convene cerrar el driver para liberar recursos
     driver.close()
 
 
-lat, lon = devuelve_coordenadas()
+lat, lon, address = devuelve_coordenadas()
 
-catastro(lat,lon)
+catastro(lat,lon,address)
